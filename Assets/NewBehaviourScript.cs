d7832039441c5a61b422e32a8951179558cdd25b
ACEDIA2567 <sdman2567@gmail.com>
2024-05-03 23:01:16 +0900

    test: TextChange

--- Assets/NewBehaviourScript.cs
+++ Assets/NewBehaviourScript.cs
@@ -7,7 +7,7 @@ public class NewBehaviourScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("aaaaaa");
+        Debug.Log("aaa   aaa");
     }
 
     // Update is called once per frame
